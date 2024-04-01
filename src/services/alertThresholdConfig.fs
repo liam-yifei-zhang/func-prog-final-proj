df25d7a7931141c280f85b57c0c5adbc900a7960
weemps <shengan2@illinois.edu>
2024-04-01 04:10:06 -0500

    four workflows: real time, process order, alert set, annualized

--- src/services/alertThresholdConfig.fs
+++ src/services/alertThresholdConfig.fs
@@ -1,5 +1,5 @@
 type Input = 
-    | UserConfiguresAlertThreshold of UserSetsAlertThreshold | UserUpdatesAlertThreshold
+    | UserConfiguresAlertThreshold of UserSetsAlertThreshold 
     | UserRequestsThresholdReset of UserInvokesThresholdReset
 
 type Output = 
@@ -7,32 +7,54 @@ type Output =
     | AlertThresholdUpdateFailed of AlertThresholdUpdateNotSuccessful
     | AlertThresholdUpdated of ThresholdReset
 
-let setAlertThreshold (input: Input) =
-    match input with
-    | UserConfiguresAlertThreshold config ->
-        validateUserInput config
-        |> function
-            | true ->
-                updateAlertThresholdInDatabase config
-                |> fun _ -> 
-                    match config.AutoStopTrading with
-                    | true -> updateAutoStopTradingFlagInDatabase config
-                    | false -> ()
-                AlertThresholdSet AlertThresholdUpdatedInDatabase
-                |> emit
-            | false -> AlertThresholdUpdateFailed AlertThresholdUpdateNotSuccessful |> emit
-    | _ -> ()
-
-let resetAlertThreshold (input: Input) =
-    match input with
-    | UserRequestsThresholdReset _ ->
-        resetAlertThresholdInSystem()
-        |> fun _ -> AlertThresholdUpdated ThresholdReset |> emit
-    | _ -> ()
-
-let workflowConfigureAlertThreshold () =
-    getInputEvent()
+let setAlertThreshold (input: UserSetsAlertThreshold) =
+    validateUserInput input
     |> function
-        | UserConfiguresAlertThreshold _ as input -> setAlertThreshold input
-        | UserRequestsThresholdReset _ as input -> resetAlertThreshold input
-        | _ -> printfn "Unsupported operation"
+        | true ->
+            updateAlertThresholdInDatabase input
+            |> fun result -> 
+                match result with
+                | UpdateSuccessful ->
+                    match input.AutoStopTrading with
+                    | true -> 
+                        updateAutoStopTradingFlagInDatabase input
+                        AlertThresholdSet AlertThresholdUpdatedInDatabase |> emit
+                    | false -> 
+                        AlertThresholdSet AlertThresholdUpdatedInDatabase |> emit
+                | UpdateFailed ->
+                    AlertThresholdUpdateFailed AlertThresholdUpdateNotSuccessful |> emit
+        | false -> 
+            AlertThresholdUpdateFailed AlertThresholdUpdateNotSuccessful |> emit
+
+let resetAlertThreshold () =
+    resetAlertThresholdInSystem()
+    |> function
+        | ResetSuccessful -> AlertThresholdUpdated ThresholdReset |> emit
+        | ResetFailed -> AlertThresholdUpdateFailed AlertThresholdUpdateNotSuccessful |> emit
+
+let workflowConfigureAlertThreshold (input: Input) =
+    match input with
+        | UserConfiguresAlertThreshold config -> setAlertThreshold config
+        | UserRequestsThresholdReset _ -> resetAlertThreshold ()
+        | _ -> printfn "Domain error: unsupported operation"
+
+// Mock types and functions for completeness of the example
+type UserSetsAlertThreshold = {
+    ThresholdValue: decimal
+    AutoStopTrading: bool
+}
+
+type UserInvokesThresholdReset = unit
+
+type AlertThresholdUpdatedInDatabase = unit
+type AlertThresholdUpdateNotSuccessful = unit
+type ThresholdReset = unit
+
+type UpdateResult = UpdateSuccessful | UpdateFailed
+type ResetResult = ResetSuccessful | ResetFailed
+
+// let validateUserInput (_: UserSetsAlertThreshold) = true
+// let updateAlertThresholdInDatabase (_: UserSetsAlertThreshold) = UpdateSuccessful
+// let updateAutoStopTradingFlagInDatabase (_: UserSetsAlertThreshold) = ()
+// let resetAlertThresholdInSystem () = ResetSuccessful
+// let getInputEvent () = UserConfiguresAlertThreshold { ThresholdValue = 100m; AutoStopTrading = true }
