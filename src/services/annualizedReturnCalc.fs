a0bc22d895e4bdec047e8b90d1101d0697c92cfc
weemps <shengan2@illinois.edu>
2024-04-01 04:55:50 -0500

    separate pure

--- src/services/annualizedReturnCalc.fs
+++ src/services/annualizedReturnCalc.fs
@@ -1,5 +1,6 @@
 module AnnualizedReturnCalculator =
     open System
+    open Core.CalcAnnualizedReturn
 
     type CashFlow = {
         Inflow: decimal
@@ -33,19 +34,6 @@ module AnnualizedReturnCalculator =
             return { Period = { StartDate = startDate; EndDate = endDate }; DayOneCashFlows = dayOneCashFlows; FinalValue = finalValue }
         }
 
-    let calculateInvestmentDuration (period: InvestmentPeriod) : decimal =
-        let totalDays = (period.EndDate - period.StartDate).TotalDays
-        totalDays / 365m
-
-    let calculateInitialInvestment (cashFlows: CashFlow) : decimal =
-        cashFlows.Inflow - cashFlows.Outflow
-
-    let calculateAnnualizedReturn (initialInvestment: decimal) (finalValue: decimal) (years: decimal) : decimal =
-        match years > 0m with
-        | true ->
-            let growthRate = finalValue / initialInvestment
-            (growthRate ** (1m / years)) - 1m
-        | false -> 0m
 
     let emitAnnualizedReturnCalculatedEvent (annualizedReturn: decimal) =
         printfn "Annualized Return Calculated: %A" annualizedReturn
