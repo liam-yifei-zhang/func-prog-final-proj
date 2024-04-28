9685ea2addb4264bb7b202d48c74055e1e5e1659
SimonSun22 <84614775+SimonSun22@users.noreply.github.com>
2024-04-27 17:44:38 -0700

    update main

--- Main.fs
+++ Main.fs
@@ -7,6 +7,7 @@ open MongoDBUtil
 // 1. Make sure you add related files in the .fsproj file like this: <Compile Include="../src/infra/MongoDBUtil.fs" />
 // 2. After you made changes in the .fsproj file, run "dotnet build"
 // 3. You can run the program with "dotnet run" or "dotnet run --project src/main.fsproj"
+    // 3.1 make sure you run "dotnet add package MongoDB.Driver" before using MongoDBUtil
 
 [<EntryPoint>]
 let main argv =
