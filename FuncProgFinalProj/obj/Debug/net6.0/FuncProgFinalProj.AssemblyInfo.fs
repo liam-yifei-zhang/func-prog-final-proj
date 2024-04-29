842b7386c0c3d897e603d2b3f13561a79363c1f8
weemps <shengan2@illinois.edu>
2024-04-29 07:57:57 -0500

    readme updated

--- FuncProgFinalProj/obj/Debug/net6.0/FuncProgFinalProj.AssemblyInfo.fs
+++ FuncProgFinalProj/obj/Debug/net6.0/FuncProgFinalProj.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("FuncProgFinalProj")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30af5217f35c5441ce7b5ab874521445cfe8390f")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("FuncProgFinalProj")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("FuncProgFinalProj")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
